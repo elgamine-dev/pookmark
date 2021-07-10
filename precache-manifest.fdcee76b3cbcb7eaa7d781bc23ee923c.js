34461d586a8b299b652403ed58690df45e93ce4d
elgamine-dev <aelghayate@simplonprod.co>
2021-07-10 15:21:21 +0000

    Deploying to gh-pages from @ elgamine-dev/pookmark@efddf0307cb999ac72a7f13287fd50a17d6f269e 🚀

---
+++ precache-manifest.fdcee76b3cbcb7eaa7d781bc23ee923c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d3869d20e26916c8b06ee0af94fcc8",
+    "url": "/pookmark/index.html"
+  },
+  {
+    "revision": "63d732233cc3cf9a757c",
+    "url": "/pookmark/static/css/2.4d135d0b.chunk.css"
+  },
+  {
+    "revision": "746d1dcb7c32be456963",
+    "url": "/pookmark/static/css/main.ce519ec5.chunk.css"
+  },
+  {
+    "revision": "63d732233cc3cf9a757c",
+    "url": "/pookmark/static/js/2.aa5703ed.chunk.js"
+  },
+  {
+    "revision": "352a45dbe125e79623df22a2fa7ab4d3",
+    "url": "/pookmark/static/js/2.aa5703ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746d1dcb7c32be456963",
+    "url": "/pookmark/static/js/main.80fa10e1.chunk.js"
+  },
+  {
+    "revision": "ba812d7c210ecfa894b2",
+    "url": "/pookmark/static/js/runtime-main.98e1e8bb.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/pookmark/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/pookmark/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/pookmark/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/pookmark/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/pookmark/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/pookmark/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/pookmark/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/pookmark/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/pookmark/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/pookmark/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/pookmark/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/pookmark/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/pookmark/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/pookmark/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/pookmark/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/pookmark/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
